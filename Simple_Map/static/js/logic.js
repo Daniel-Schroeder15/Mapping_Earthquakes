1842939392efac46d3ad267f9a9164aced0a6aca
Daniel Schroeder <Daniel.Schroeder15@outlook.com>
2020-06-23 16:20:22 -0500

    push logic.js

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -19,3 +19,9 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// 300 pixel radius of central LA
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color:"black",
+  fillColor: '#ffffa1'
+}).addTo(map);
